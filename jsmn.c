4ce44040579e65755d14a3d7dabdae0913959a24
pt300 <pt300@tlen.pl>
2016-10-01 18:19:20 +0200

    Seems to actually fix zserge/jsmn#81

--- jsmn.c
+++ jsmn.c
@@ -198,7 +198,7 @@ int jsmn_parse(jsmn_parser *parser, const char *js, size_t len,
 						break;
 					}
 					if (token->parent == -1) {
-						if(token->type != type) {
+						if(token->type != type || parser->toksuper == -1) {
 							return JSMN_ERROR_INVAL;
 						}
 						break;
