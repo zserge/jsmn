b222fe9cc60fbf7479c6ac9686c3566278993a2b
Your Name <you@example.com>
2018-06-06 21:32:29 +0900

    example3

--- mysource/filesimple.c
+++ mysource/filesimple.c
@@ -9,81 +9,85 @@
  */
  static char * readJSONFile(){
  //	char *JSON_STRING="{\"u\": \"dd\"}";
-
  	char oneline[255];
-	char * c = (char *)malloc(255);
+	char * c = (char *)malloc(600);
 	FILE *fp = fopen("/home/u21000127/jsmn/data.json","r");	// data.json 읽기 모드로 열기
-  while(fgets(c,sizeof(c),fp)!= NULL)
-    printf("%s",c);
+  while(fgets(oneline,255,fp)!= NULL){
+    strcat(c,oneline);
+  }
   if(feof(fp)!=0)
-    puts("복사 완료");
+  //  puts("복사 완료");
   fclose(fp);
+
+  return c;
  }
 
 // static const char *JSON_STRING =
 // 	"{\"user\": \"johndoe\", \"admin\": false, \"uid\": 1000,\n  "
 // 	"\"groups\": [\"users\", \"wheel\", \"audio\", \"video\"]}";
-//
-// static int jsoneq(const char *json, jsmntok_t *tok, const char *s) {
-// 	if (tok->type == JSMN_STRING && (int) strlen(s) == tok->end - tok->start &&
-// 			strncmp(json + tok->start, s, tok->end - tok->start) == 0) {
-// 		return 0;
-// 	}
-// 	return -1;
-// }
+
+static int jsoneq(const char *json, jsmntok_t *tok, const char *s) {
+	if (tok->type == JSMN_STRING && (int) strlen(s) == tok->end - tok->start &&
+			strncmp(json + tok->start, s, tok->end - tok->start) == 0) {
+		return 0;
+	}
+	return -1;
+}
 
 int main() {
   char *JSON_STRING = readJSONFile();
-	// int i;
-	// int r;
-	// jsmn_parser p;
-	// jsmntok_t t[128]; /* We expect no more than 128 tokens */
-  //
-	// jsmn_init(&p);
-	// r = jsmn_parse(&p, readJSONFile(), strlen(readJSONFile()), t, sizeof(t)/sizeof(t[0]));
-	// if (r < 0) {
-	// 	printf("Failed to parse JSON: %d\n", r);
-	// 	return 1;
-	// }
-  //
-	// /* Assume the top-level element is an object */
-	// if (r < 1 || t[0].type != JSMN_OBJECT) {
-	// 	printf("Object expected\n");
-	// 	return 1;
-	// }
-  //
-	// /* Loop over all keys of the root object */
-	// for (i = 1; i < r; i++) {
-	// 	if (jsoneq(readJSONFile(), &t[i], "user") == 0) {
-	// 		/* We may use strndup() to fetch string value */
-	// 		printf("- User: %.*s\n", t[i+1].end-t[i+1].start,
-	// 				readJSONFile() + t[i+1].start);
-	// 		i++;
-	// 	} else if (jsoneq(readJSONFile(), &t[i], "admin") == 0) {
-	// 		/* We may additionally check if the value is either "true" or "false" */
-	// 		printf("- Admin: %.*s\n", t[i+1].end-t[i+1].start,
-	// 				readJSONFile() + t[i+1].start);
-	// 		i++;
-	// 	} else if (jsoneq(readJSONFile(), &t[i], "uid") == 0) {
-	// 		/* We may want to do strtol() here to get numeric value */
-	// 		printf("- UID: %.*s\n", t[i+1].end-t[i+1].start,
-	// 				readJSONFile() + t[i+1].start);
-	// 		i++;
-	// 	} else if (jsoneq(readJSONFile(), &t[i], "groups") == 0) {
-	// 		int j;
-	// 		printf("- Groups:\n");
-	// 		if (t[i+1].type != JSMN_ARRAY) {
-	// 			continue; /* We expect groups to be an array of strings */
-	// 		}
-	// 		for (j = 0; j < t[i+1].size; j++) {
-	// 			jsmntok_t *g = &t[i+j+2];
-	// 			printf("  * %.*s\n", g->end - g->start, readJSONFile() + g->start);
-	// 		}
-	// 		i += t[i+1].size + 1;
-	// 	} else {
-	// 		printf("Unexpected key: %.*s\n", t[i].end-t[i].start,
-	// 				readJSONFile() + t[i].start);
-	// 	}
-	// }
-	// return EXIT_SUCCESS;
+//  printf("%s\n", JSON_STRING );
+	int i;
+	int r;
+	jsmn_parser p;
+	jsmntok_t t[128]; /* We expect no more than 128 tokens */
+
+	jsmn_init(&p);
+	r = jsmn_parse(&p, JSON_STRING, strlen(JSON_STRING), t, sizeof(t)/sizeof(t[0]));
+	if (r < 0) {
+		printf("Failed to parse JSON: %d\n", r);
+		return 1;
+	}
+
+	/* Assume the top-level element is an object */
+	if (r < 1 || t[0].type != JSMN_OBJECT) {
+		printf("Object expected\n");
+		return 1;
+	}
+
+	/* Loop over all keys of the root object */
+	for (i = 1; i < r; i++) {
+		if (jsoneq(JSON_STRING, &t[i], "name") == 0) {
+			/* We may use strndup() to fetch string value */
+			printf("- name: %.*s\n", t[i+1].end-t[i+1].start,
+					JSON_STRING + t[i+1].start);
+			i++;
+		} else if (jsoneq(JSON_STRING, &t[i], "keywords") == 0) {
+			/* We may additionally check if the value is either "true" or "false" */
+			printf("- keywords: %.*s\n", t[i+1].end-t[i+1].start,
+					JSON_STRING + t[i+1].start);
+			i++;
+		} else if (jsoneq(JSON_STRING, &t[i], "description") == 0) {
+			/* We may want to do strtol() here to get numeric value */
+			printf("- UID: %.*s\n", t[i+1].end-t[i+1].start,
+					JSON_STRING + t[i+1].start);
+			i++;
+		} else if (jsoneq(JSON_STRING, &t[i], "examples") == 0) {
+			int j;
+			printf("- examples:\n");
+			if (t[i+1].type != JSMN_ARRAY) {
+				continue; /* We expect groups to be an array of strings */
+			}
+			for (j = 0; j < t[i+1].size; j++) {
+				jsmntok_t *g = &t[i+j+2];
+				printf("  * %.*s\n", g->end - g->start, JSON_STRING + g->start);
+			}
+			i += t[i+1].size + 1;
+		} else {
+			// printf("Unexpected key: %.*s\n", t[i].end-t[i].start,
+			// 		JSON_STRING + t[i].start);
+		}
+	}
+	return EXIT_SUCCESS;
+  free(JSON_STRING);
 }
