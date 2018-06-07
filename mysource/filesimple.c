c38ba95f4d3b1e566a89eded61bb472f06f01166
Your Name <you@example.com>
2018-06-07 17:04:54 +0900

    exmaple6

--- mysource/filesimple.c
+++ mysource/filesimple.c
@@ -23,7 +23,6 @@
  }
 
 void jsonNameList(char *jsonstr, jsmntok_t *t, int tokcount, int *nameTokIndex ){
-    printf("**** Name List ****\n");
     int i,count=1;
 //    nameTokIndex = (int *)malloc(sizeof(int)*100);
     for(i=0;i<tokcount;i++){ // 100 개까지만
@@ -36,6 +35,7 @@ void jsonNameList(char *jsonstr, jsmntok_t *t, int tokcount, int *nameTokIndex )
 }
 
 void printNameList(char *jsonstr, jsmntok_t *t, int *nameTokIndex){
+    printf("**** Name List ****\n");
     int i;
     for(i=0;i<nameTokIndex[0];i++){
       printf("[NAME %d]  %.*s\n", i+1, t[nameTokIndex[i+1]].end-t[nameTokIndex[i+1]].start,
@@ -44,6 +44,28 @@ void printNameList(char *jsonstr, jsmntok_t *t, int *nameTokIndex){
 
 }
 
+char * noValue(char *jsonstr, jsmntok_t *t, int *nameTokIndex, int num){
+    int size = t[nameTokIndex[num]+1].end-t[nameTokIndex[num]+1].start;
+    char * value = (char *)malloc(size);
+    strncpy(value, jsonstr+t[nameTokIndex[num]+1].start, size);
+    return value;
+}
+
+void selectNameList(char *jsonstr, jsmntok_t *t, int *nameTokIndex){
+
+    int num;
+    char *value;
+    while(1){
+      printf("Select Name's no (exit:0) >>");
+      scanf(" %d", &num);
+      if(num == 0) break;
+      value = noValue(jsonstr,t,nameTokIndex,num);
+      printf("[NAME %d]  %.*s\n %s\n\n", num, t[nameTokIndex[num]].end-t[nameTokIndex[num]].start,
+      jsonstr+t[nameTokIndex[num]].start, value );
+
+    }
+
+}
 // void jsonNameList(char *jsonstr, jsmntok_t *t, int tokcount){
 //     printf("**** Name List ****\n");
 //     int i,count=1;
@@ -77,11 +99,12 @@ int main() {
 
 	jsmn_init(&p);
 	r = jsmn_parse(&p, JSON_STRING, strlen(JSON_STRING), t, sizeof(t)/sizeof(t[0]));
+  // parse해야 t 배열에 토큰들 담김
 
   int arr[100];
   jsonNameList(JSON_STRING,t,100,arr);
-  printNameList(JSON_STRING,t,arr);
-
+//  printNameList(JSON_STRING,t,arr);
+  selectNameList(JSON_STRING,t,arr);
 	// if (r < 0) {
 	// 	printf("Failed to parse JSON: %d\n", r);
 	// 	return 1;
