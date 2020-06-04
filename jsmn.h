6bc659404f8c91a9f3c150f248302d406d470d23
Dominick C. Pastore <dominickpastore@dcpx.org>
2020-06-04 14:45:07 -0400

    Remove use of strlen libc call

--- jsmn.h
+++ jsmn.h
@@ -208,7 +208,7 @@ static int jsmn_parse_primitive(jsmn_parser *parser, const char *js,
 #ifdef JSMN_STRICT
   if (js[parser->pos] == 't') {
     for (i = 1;
-         i < strlen(true_str);
+         i < sizeof(true_str) - 1;
          i++) {
       if (parser->pos + i >= len || js[parser->pos + i] == '\0') {
         return JSMN_ERROR_PART;
@@ -219,7 +219,7 @@ static int jsmn_parse_primitive(jsmn_parser *parser, const char *js,
     parser->pos += 4;
   } else if (js[parser->pos] == 'f') {
     for (i = 1;
-         i < strlen(false_str);
+         i < sizeof(false_str) - 1;
          i++) {
       if (parser->pos + i >= len || js[parser->pos + i] == '\0') {
         return JSMN_ERROR_PART;
@@ -230,7 +230,7 @@ static int jsmn_parse_primitive(jsmn_parser *parser, const char *js,
     parser->pos += 5;
   } else if (js[parser->pos] == 'n') {
     for (i = 1;
-         i < strlen(null_str);
+         i < sizeof(null_str) - 1;
          i++) {
       if (parser->pos + i >= len || js[parser->pos + i] == '\0') {
         return JSMN_ERROR_PART;
